03f7ee705a7934243dac5915981c74bd8bd4c4a6
baluogit <f614746841@gamil.com>
2024-09-18 19:17:10 +0800

    异步1、2

---
+++ asynctest/asynctest/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace asynctest
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string filename = @"D:\study\C#\file\a.txt";
+            await File.AppendAllTextAsync(filename, "hello");
+            string s = File.ReadAllText(filename);
+            //string s = await File.ReadAllTextAsync(filename);
+            Console.WriteLine(s);
+        }
+    }
+}
